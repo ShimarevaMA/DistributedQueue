8cb47f518f94068ce2fa44dcf8f9878d65f2a95c
Рита <@DESKTOP-HME2BOS>
2024-11-22 17:03:50 +0300

    Добавьте файлы проекта.

---
+++ DistributedQueue.Common/Interface1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace DistributedQueue.Common
+{
+    public interface SumTo50
+    {
+        Task Sum50(string name, CancellationToken token);
+    }
+}
---
+++ DistributedQueue.Worker/Class1.cs
@@ -0,0 +1,36 @@
+﻿using DistributedQueue.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DistributedQueue.Worker
+{
+    public class ComputeSum : SumTo50
+    {
+        public Task Sum50(string name, CancellationToken token) {
+            var startTime = DateTime.Now;
+            Console.Write($"{startTime}: Started compute: ");
+            int sum = 0;
+
+            for (int i = 1; i <= 50; i++)
+            {
+                sum += i;
+                Console.WriteLine($"Сумма чисел от 1 до {i}: {sum}");
+
+                if (token.IsCancellationRequested)
+                {
+                    Console.WriteLine($"{DateTime.Now}: Sum: Cancelled compute sum {name}");
+                    break;
+                }
+            }
+
+            Console.WriteLine($"ComputeSum: {name}, task completed");
+
+            return Task.CompletedTask;
+
+        }
+    }
+}
